3ec138b3dcafa1b20e2adba09ed22c19f5d04abe
kwilliams300 <kwilliams300@gmail.com>
2017-03-12 12:32:46 +0800

    Nav hamburger

--- js/main.js
+++ js/main.js
@@ -1,4 +1,10 @@
 $(document).ready(function(){
   $(".hidden").removeClass("hidden")
   //JQuery goes home-banner-content
+
+   $(".burger-button").click(function(){
+     $(".burger-button").toggleClass("active");
+     $(".burger-menu").slideToggle();
+   });
+
 });
